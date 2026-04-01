78ee814829b2b1e7e7d499855c8e8e5f41d64b5a
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2026-04-01 17:08:46 -0400

    Initial Commit

---
+++ wakatime-cli/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wakatime-cli
+//
+//  Created by Adithiya Venkatakrishnan on 1/4/2026.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return EXIT_SUCCESS;
+}
